8f907cfa4065006df4ce01142ab4d40684bb7bae
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-03-24 01:45:00 +0200

    enclosed scope added

--- functionsandvariablescopes.py
+++ functionsandvariablescopes.py
@@ -1,9 +1,20 @@
 # local usage, refers to a variable declared inside a function.
 def add_nums(a, b):
-
     answer = a + b
     return answer
-print(add_nums(40, 50))
 
 
+print(add_nums(40, 40))
 
+
+# Enclosing scope, a function inside another function also called a nested function.
+def add_nums(a, b):
+    answer = a + b
+
+    def double_it():
+        double = answer * 2
+        print(double)
+        double_it()
+        return answer
+
+    print(add_nums(2, 13))
